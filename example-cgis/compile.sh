233c48b5468f7313b8ef6ce89d6b9732501290ec
floyd-fuh <floyd@floyd.ch>
2016-04-07 18:13:54 +0200

    More changes

--- example-cgis/compile.sh
+++ example-cgis/compile.sh
@@ -1,3 +1,2 @@
-afl-clang -g -o simple_print_env.elf simple_print_env.c
-afl-clang -g -o simple_accept_and_append.elf simple_accept_and_append.c
-afl-clang -g -o simple_form.elf simple_form.c
+afl-clang -g -o open_source_simple_print_env.elf simple_print_env.c
+clang -o closed_source_simple_print_env.elf simple_print_env.c
